6faf1aefb705a161b08bb6d255e506e2d77da85c
Muhammad-VII <meshoronaldo62@gmail.com>
2021-06-15 01:28:52 +0200

    1.2.0

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,32 +1,39 @@
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
+declare let $: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  
-  isLogin:boolean = false;
-  constructor(private _AuthService:AuthService) { 
+  isLogin: boolean = false;
+  constructor(private _AuthService: AuthService) {
     _AuthService.currentUser.subscribe(() => {
-      if(_AuthService.currentUser.getValue() != null){
+      if (_AuthService.currentUser.getValue() != null) {
         this.isLogin = true;
-      }
-      else
-      {
+      } else {
         this.isLogin = false;
+      }
+    });
+  }
+
+  isLogOut() {
+    this._AuthService.logout();
+  }
+
+  ngOnInit(): void {
+    let tvOffset = 700
+    $(window).on('scroll', () => {
+      let windowScroll = $(window).scrollTop();
+      console.log(windowScroll);
+      if (windowScroll > tvOffset) {
+        $('#main-nav').css('backgroundColor','#1317225b').css(`box-shadow`,`none`)
+      } else {
+        $('#main-nav').css('backgroundColor', '#131722').css(`box-shadow`,`rgb(50, 50, 93) 0px 14px 17px -5px,
+        rgba(0, 0, 0, 0.486) 0px 8px 11px -8px`)
       }
     })
   }
-
-  isLogOut(){
-    this._AuthService.logout()
-  }
-  
-  ngOnInit(): void {
-    
-  }
-
 }
